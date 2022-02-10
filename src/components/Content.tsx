7f06f48c4a5be09d10dc532629cc5864277df63b
Murilo <pinheiromurilo13@gmail.com>
2022-02-10 11:35:26 -0300

    SideBar, Content e Header Componetizados

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,42 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  selectedGenreId: number;  
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
+export function Content({ selectedGenreId }: ContentProps) {
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+    console.log(`selectedGenreId: ${selectedGenreId}`)
+  }, [selectedGenreId]);
+
+      return (
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+       </main>
+      )        
+
 }
